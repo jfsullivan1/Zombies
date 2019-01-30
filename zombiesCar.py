22ee2d352af4d8ab3162530b1dbb19898652dc45
John Sullivan <jfsullivan1@my.uri.edu>
2019-01-30 09:16:53 -0500

    Update zombiesCar.py
    car file

--- zombiesCar.py
+++ zombiesCar.py
@@ -7,7 +7,7 @@ pygame.init()
 class Vehicle(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("car7.jpg")
+        self.image = pygame.image.load("car4.png")
         self.image = self.image.convert()
         self.image = pygame.transform.scale(self.image, (40, 60))
         self.rect = self.image.get_rect()
