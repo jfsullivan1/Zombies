1157f46f7d995852a05f567c69397713de231a6b
John Sullivan <jfsullivan1@my.uri.edu>
2019-02-04 12:03:41 -0500

    more stuff
    updated images, has a gun now

--- carAndGun.py
+++ carAndGun.py
@@ -10,69 +10,103 @@ pygame.init()
 
 #Car Class (Player)
 class Vehicle(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, gun):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("car7.png")
         self.image = self.image.convert()
         self.image = pygame.transform.scale(self.image, (80, 120))
         self.rect = self.image.get_rect()
-        self.dx = 8
-        self.dy = 8
+        self.gun = gun
+        self.dx = 12
+        self.dy = 12
         self.rect.center = (400, 400)
+
+    #Function to make the car turn up on the screen
     def up(self):
         self.rect.centery -= self.dy
         self.image = pygame.image.load("car7.png")
         self.image = self.image.convert()
         self.image = pygame.transform.scale(self.image, (80, 120))
+
+    #Turn down
     def down(self):
-        #self.rect.centery += self.dy
-        self.rect.centerx -= self.dx
+        self.rect.centery += self.dy
         self.image = pygame.image.load("carDown.png")
         self.image = self.image.convert()
         self.image = pygame.transform.scale(self.image, (80, 120))
+
+    #Turn left
     def right(self):
         self.rect.centerx += self.dx
         self.image = pygame.image.load("carRight.png")
         self.image = self.image.convert()
         self.image = pygame.transform.scale(self.image, (80, 120))
-        
+
+    #Turn right
     def left(self):
         self.rect.centerx -= self.dx
         self.image = pygame.image.load("carLeft.png")
         self.image = self.image.convert()
         self.image = pygame.transform.scale(self.image, (80, 120))
 
+    #Shoot Gun
+    def shootGun(self):
+        shoot = gunDefault(self.rect.centerx, self.rect.top, self.gun)
+        self.gun.add(shoot)
+
 #Gun Default Class
 class gunDefault(pygame.sprite.Sprite):
-    def __init__(self, carX, carY, gunGroup):
+    def __init__(self, startingX, startingY, gun):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((4, 12))
+        self.image = pygame.image.load("bullet.png")
         self.image = self.image.convert()
-        self.image.fill((211, 211, 211))
+        self.image = pygame.transform.scale(self.image, (10, 25))
         self.rect = self.image.get_rect()
-        self.rect.centerx = carX
-        self.rect.centery = carY
+        self.rect.centerx = startingX
+        self.rect.centery = startingY
         self.dy = -15
-        self.gunGroup = gunGroup
+        self.gun = gun
 
     def update(self):
         self.rect.centery += self.dy
         if self.rect.bottom < 0:
             self.dy = 0
-            self.gunGroup.remove(self)
+            self.gun.remove(self)
+
+class BackGround(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        self.dy = 5
+        self.reset()
+
+    def update(self):
+        self.rect.bottom += self.dy
+        if self.rect.top >= 0:
+            self.reset()
+
+    def reset(self):
+        self.rect.bottom = screen.get_height()
 
 #MAIN FUNCTION (Will separate into a game() function later)
 def main():
     
     #This code sets up the screen and fills the background with a color
-    screen = pygame.display.set_mode((900, 800))
+    screen = pygame.display.set_mode((1600, 820))
     pygame.display.set_caption("Zombies")
     backDrop = pygame.Surface(screen.get_size())
     backDrop.fill((0, 0, 0))
     screen.blit(backDrop, (0,0))
 
-    #This assigns the car sprite to a variable
-    car = Vehicle()
+    gunGRP = pygame.sprite.Group()
+    
+    #This assigns the car sprite to a variable (for ease of use)
+    car = Vehicle(gunGRP)
+    
+    #This assigns the gun sprite to a variable (for ease of use)
+    #gun = gunDefault(car)
     
     #This is necessary for collisions later on
     spriteGRP = pygame.sprite.Group(car)
@@ -83,6 +117,8 @@ def main():
     #Variable to keep game running if person doesn't quit.
     donePlaying = False
 
+
+    
     #Game loop!
     while donePlaying == False:
         clock.tick(30)
@@ -91,7 +127,8 @@ def main():
         for event in pygame.event.get():
             if(event.type==pygame.QUIT):
                 donePlaying = True
-    
+
+        
         #This identifies what key is being pressed and associates some action
         # with it. 
         key = pygame.key.get_pressed()
@@ -106,10 +143,19 @@ def main():
         if key[pygame.K_DOWN]:
             car.down()
 
+        #Shoot default gun
+        if key[pygame.K_SPACE]:
+            car.shootGun()
         
 
         spriteGRP.clear(screen, backDrop)
+        gunGRP.clear(screen, backDrop)
+
+        spriteGRP.update()
+        gunGRP.update()
+        
         spriteGRP.draw(screen)
+        gunGRP.draw(screen)
         pygame.display.flip()
 
     pygame.mouse.set_visible(True)
